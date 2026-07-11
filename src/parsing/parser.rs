20486767f2c3fa3d9fcbdb48a2636f1c6902b6a9
Stefano Baghino <stefano@baghino.me>
2026-07-11 16:46:55 +0200

    Un-ignore zsh-for-markdown shebang test; CI has the submodule

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -8348,7 +8348,6 @@ contexts:
     /// strict `main`). The parent merge must prefer the own definition
     /// over the inherited one.
     #[test]
-    #[ignore = "requires testdata/Packages submodule"]
     fn zsh_for_markdown_uses_lenient_shebang_main_from_bash_for_markdown() {
         let ss = SyntaxSet::load_from_folder("testdata/Packages").unwrap();
         let md = ss
